a98d04b88215e82996e962465797062e3c741cb2
Ryan <ryeean16@gmail.com>
2023-10-20 03:47:15 -0700

    added database connection functionality for teacher sign up

--- server.js
+++ server.js
@@ -2,45 +2,47 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
-const qs = require('qs');
 const app = express();
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 5050;
-const EXTERNAL_API_URL = 'https://online-tutoring-system-api.herokuapp.com';
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));  // This will handle URL-encoded data
 
-app.post("/api/v1/tutors/signup", (req, res) => {
-    console.log('Received request:', req.body); // Log the received request
-    axios.post(`${EXTERNAL_API_URL}/api/v1/tutors/signup`, qs.stringify(req.body))
-        .then(externalResponse => {
-            res.status(externalResponse.status).send(externalResponse.data);
-        })
-        .catch(error => {
-            console.error("Error interacting with external API:", error.response ? error.response.data : error.message);
-            res.status(500).send("Error interacting with external API. " + (error.response ? error.response.data : error.message));
-        });
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'tooter'
+  });
+  
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to the database:', err.stack);
+      return;
+    }
+    console.log('Connected to the database');
+  });
+
+  app.post("/api/v1/tutors/signup", (req, res) => {
+    const { firstname, lastname, email, password, experience } = req.body;
+
+    const query = `
+      INSERT INTO tutors (firstname, lastname, email, password, experience)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    connection.query(query, [firstname, lastname, email, password, experience], (err, results) => {
+        if (err) {
+            console.error('Error inserting data:', err);
+            return res.status(500).send({ message: "Error inserting data into database" });
+        }
+        console.log('Data inserted, ID:', results.insertId);
+        res.status(200).send({ message: "Signup data received and saved!" });
+    });
 });
 
-
-app.get("/api/v1/tutors/login", (req, res) => {
-    axios.get(`${EXTERNAL_API_URL}/api/v1/tutors/login`)
-        .then(externalResponse => {
-            res.status(externalResponse.status).send(externalResponse.data);
-        })
-        .catch(error => {
-            res.status(500).send("Error interacting with external API.");
-        });
-});
 
 app.listen(PORT, () => {
     console.log(`Express app running on port: ${PORT}`)
